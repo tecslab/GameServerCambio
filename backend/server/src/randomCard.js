451ce1a9ba24812159e6761cb581f93ccc64af49
Pablo Villota <pvillota@cuenca.gob.ec>
2022-01-02 20:18:31 -0500

    añade efecto de las cartas

--- backend/server/src/randomCard.js
+++ backend/server/src/randomCard.js
@@ -2,21 +2,21 @@
 // set of object with probabilities:
 //deberían sumar 1 si se busca que sea preciso
 let cardsSet = [
-    {name:'1', probabilidad: 4 / 54, costo:1},
-    {name:'2', probabilidad: 4 / 54, costo:2},
-    {name:'3', probabilidad: 4 / 54, costo:3},
-    {name:'4', probabilidad: 4 / 54, costo:4},
-    {name:'5', probabilidad: 4 / 54, costo:5},
-    {name:'6', probabilidad: 4 / 54, costo:6},
-    {name:'7', probabilidad: 4 / 54, costo:7},
-    {name:'8', probabilidad: 4 / 54, costo:8},
-    {name:'9', probabilidad: 4 / 54, costo:9},
-    {name:'10', probabilidad: 4 / 54, costo:10},
-    {name:'J', probabilidad: 4 / 54, costo:10},
-    {name:'Q', probabilidad: 4 / 54, costo:10},
-    {name:'KN', probabilidad: 2 / 54, costo:13},
-    {name:'KR', probabilidad: 2 / 54, costo:-1},
-    {name:'JKR', probabilidad: 2 / 54, costo:0},
+    {name:'1', probabilidad: 4 / 54, costo:1, efecto:'None'},
+    {name:'2', probabilidad: 4 / 54, costo:2, efecto:'None'},
+    {name:'3', probabilidad: 4 / 54, costo:3, efecto:'None'},
+    {name:'4', probabilidad: 4 / 54, costo:4, efecto:'None'},
+    {name:'5', probabilidad: 4 / 54, costo:5, efecto:'None'},
+    {name:'6', probabilidad: 4 / 54, costo:6, efecto:'None'},
+    {name:'7', probabilidad: 4 / 54, costo:7, efecto:'CartaAjena'},
+    {name:'8', probabilidad: 4 / 54, costo:8, efecto:'CartaAjena'},
+    {name:'9', probabilidad: 4 / 54, costo:9, efecto:'CartaPropia'},
+    {name:'10', probabilidad: 4 / 54, costo:10, efecto:'CartaPropia'},
+    {name:'J', probabilidad: 4 / 54, costo:10, efecto:'CambioCiego'},
+    {name:'Q', probabilidad: 4 / 54, costo:10, efecto:'CambioCiego'},
+    {name:'KN', probabilidad: 2 / 54, costo:13, efecto:'Cambio'},
+    {name:'KR', probabilidad: 2 / 54, costo:-1, efecto:'None'},
+    {name:'JKR', probabilidad: 2 / 54, costo:0, efecto:'None'},
 ]
 
 // Se obtiene la suma para incluir casos 
